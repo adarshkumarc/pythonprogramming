3ab91f61b58d5e8e7527bf48a82e4dacce029186
adarshgurjar <adarshgurjar744@gmail.com>
2024-04-22 08:03:31 +0530

    how to take input in python,how to use if ,else,elif condition in python

---
+++ IF_else.py
@@ -0,0 +1,8 @@
+a = 10
+b = 10
+if a < b:
+    print("a is less than b")
+elif a > b:
+    print("a is greater than b")
+else:
+    print("a is equal  b")
---
+++ inputFunc.py
@@ -0,0 +1,11 @@
+a = int(input("enter a number "))
+b = int(input("enter a number "))
+c = a + b
+print(c)
+num_int = 123
+
+num_str = "456"
+
+num_conv = int(num_str)
+
+print(num_int + num_conv)
