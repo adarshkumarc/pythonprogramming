ff8d0661a7ee70b05f7b2f4075bdb64d3f25f9c3
adarshgurjar <adarshgurjar744@gmail.com>
2024-04-23 01:15:30 +0530

    how to take input in python,how to use if ,else,elif condition in python and forloop working system

---
+++ forloop.py
@@ -0,0 +1,18 @@
+"""for x in range(10):
+    print(x)
+    print("hello world")
+    for x in range(5,10):
+    print(x)
+for x in range(1,11):
+    print(2*x)
+    print("hello world")
+num = int(input("enter a number :"))
+for x in range(1,11):
+    print(num*x)
+    """
+"""fruite=["apple","bear","cherry","mango"]
+for i in range(0,3):
+    print(i)"""
+helloworld = "hello world"
+for i in helloworld:
+    print(i)
