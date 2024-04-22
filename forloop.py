03dd3a9e0da3417f7de16cda21d77d47c4b8ba7f
adarshgurjarc <158771139+adarshkumarc@users.noreply.github.com>
2024-04-23 01:18:26 +0530

    Update forloop.py

--- forloop.py
+++ forloop.py
@@ -1,18 +1,26 @@
-"""for x in range(10):
+for x in range(10):
     print(x)
+    
     print("hello world")
+    
     for x in range(5,10):
     print(x)
+    
 for x in range(1,11):
     print(2*x)
+    
     print("hello world")
+    
 num = int(input("enter a number :"))
 for x in range(1,11):
+    
     print(num*x)
-    """
-"""fruite=["apple","bear","cherry","mango"]
+    
+fruite=["apple","bear","cherry","mango"]
 for i in range(0,3):
-    print(i)"""
+    
+    print(i)
+    
 helloworld = "hello world"
 for i in helloworld:
     print(i)
