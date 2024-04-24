a35f852ca090052e81a9e3dbc8d37578405aeea6
adarshgurjar <adarshgurjar744@gmail.com>
2024-04-25 00:08:16 +0530

    break continue in python

---
+++ breakcontinue.py
@@ -0,0 +1,24 @@
+"""for x in a:
+ print(x)
+for y in b:
+  # print(y)
+   if y==b:
+     break
+   print(y)"""
+
+"""a="hello"
+b="world"
+for x in a:
+   print(x)
+   for y in b:
+       print(y)
+       if x==y:
+           break
+           print(x,y)
+           """
+a = "hello"
+for x in a:
+    if x == "l":
+        continue
+    print(x)
+print("hello world")
